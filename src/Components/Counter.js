2fd16e2ab9609e75d4af60348726edbee6ae3269
Safwan Backar <safwanbackar29@gmail.com>
2022-06-09 15:39:37 +0530

    count increment

---
+++ src/Components/Counter.js
@@ -0,0 +1,33 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers'
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+   constructor(props) {
+     super(props)
+   
+     this.state = {
+        count: 0
+     }
+   }
+
+   increment(){
+      this.setState({
+         count: this.state.count + 1
+      })
+      console.log(this.state.count);
+   }
+
+
+
+   render(){
+      return (
+          <div>
+          <div>Count - {this.state.count}</div>
+          <button onClick={()=> this.increment()}>Increment</button>
+          </div>
+      )
+   }
+}
+
+export default Counter
