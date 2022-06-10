8b84bcbbb137b5cdbfb6bcc58d2ddcdfc532ccc5
Safwan Backar <safwanbackar29@gmail.com>
2022-06-10 10:51:36 +0530

    css 2nd mthod by style tag and 3rd by css module

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import EventBind from './Components/EventBind';
 import ParentComponent from './Components/ParentComponent';
 import UserGreeting from './Components/UserGreeting';
 import StyleSheet from './Components/StyleSheet';
+// css module method below
+import styles from './Components/appStyles.module.css'
 
 class App extends Component{
   render(){
     return(
       <div className='App'>
+        <h1 className={styles.success}>The Success Stuff</h1>
         {/* <Counter/> */}
         {/* <Greet/> */}
         {/* <FunctionClick/>
--- src/Components/StyleSheet.js
+++ src/Components/StyleSheet.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import './myStyles.css'
 
+
+// 2. method => by adding style tag to h1 , you can pass {heading} to the css
+// const heading = {
+//   fontSize : '200px',
+//   color : 'pink'
+// }
+
+
+// 1. by passing class name as props
 function StyleSheet(props) {
   let className = props.primary? 'primary' : ''
     return (
